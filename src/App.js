fda57af539839e0b30492054909a53686dbd8645
unknown <marcinoles87@gmail.com>
2022-12-01 20:17:08 +0100

    build structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task.js';
 
 function App() {
   return (
     <div className="App">
       <h1>To do List</h1>
+      <div>
+
+       <p className='task'>Zadanie do zrobienia</p>
+
+          <Task></Task>
+
+       </div>
+
     </div>
   );
 }
---
+++ src/components/Task.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Task = () => {
+    return (
+       <div>
+          <input className='input'></input>
+          <button className='button'>Add</button>
+       </div>
+      );
+}
+ 
+export default Task;
