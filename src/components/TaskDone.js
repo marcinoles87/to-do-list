a95a82c80100d75b00da174db9c6e0db033bca27
unknown <marcinoles87@gmail.com>
2022-12-07 18:16:30 +0100

    add new Task

---
+++ src/components/TaskDone.js
@@ -0,0 +1,13 @@
+import Task from "./Task";
+
+const TaskDone = (props) => {
+    return ( 
+        <>
+        <h1>Task Done</h1>
+        <Task></Task>
+        </>
+        
+     );
+}
+ 
+export default TaskDone;
